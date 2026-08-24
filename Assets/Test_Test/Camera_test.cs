8d12eb805171062e54f9262f60cdd6b7604d7b33
kalitoth <kalitoth@gmail.com>
2026-08-25 04:50:37 +0900

    Make Test Camera

---
+++ Assets/Test_Test/Camera_test.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_test : MonoBehaviour
+{
+     
+    [SerializeField]
+    private Transform _player;
+
+    [SerializeField]
+    private float _sharpness = 10;
+    private float _interpole;
+    
+    Vector3 offset = new Vector3(0,10,-5);
+    void Start()
+    {
+        _interpole = 1 - Mathf.Exp(-_sharpness);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Quaternion camToPlayer = Quaternion.LookRotation(_player.position - transform.position);
+        transform.position = Vector3.Lerp(transform.position, _player.position+ _player.rotation*offset, _interpole);
+        transform.rotation = Quaternion.Slerp(transform.rotation, camToPlayer, _interpole);
+        //transform.rotation = camToPlayer;
+    }
+}
