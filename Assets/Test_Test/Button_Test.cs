cbebefba835524474dfa8d13faba84c8e6c83590
kalitoth <kalitoth@gmail.com>
2026-08-25 19:03:29 +0900

    Test : Button Test

---
+++ Assets/Test_Test/Button_Test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Test : MonoBehaviour
+{
+    public void Buttontest1()
+    {
+        Debug.Log("Button1 is pressed");
+    }
+    public void Buttontest2()
+    {
+        Debug.Log("Button2 is pressed");
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
